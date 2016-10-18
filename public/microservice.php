c0089f88bc24b66cc339e466c5a95e6cad56a305
James Anslow <return.404@gmail.com>
2016-10-18 22:09:47 +0100

    Added base frontend for listing products

--- public/microservice.php
+++ public/microservice.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application as Microservice;
-use Httpful\Request as ShopifyHttpClient;
+use Httpful\Request as HttpClient;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
